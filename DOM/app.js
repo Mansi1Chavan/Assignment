946f3872f7bbd84137b57b5ad0945466d5248e58
Mansi1Chavan/assignment <mansichavan5050@gmail.com>
2022-11-11 15:45:33 +0530

    Hello

---
+++ DOM/app.js
@@ -0,0 +1,23 @@
+// const elements = document.getElementsByClassName("some-para");
+// // const element = document.getElementsByClassName("some-para");
+// // console.log(elements)
+// // console.log(element)
+// console.log(document);
+// const ele  = document.getElementById("some-para")
+// console.log(ele.style);
+// ele.style.backgroundColor="Yellow";
+
+
+// console.log(document);
+// const ele = document.getElementById("deg1");
+// console.log(ele);
+// const element = document.getElementsByClassName("intro");
+// console.log(element[1]);
+// const el = document.querySelector("#chavan" , "#mansi");
+// console.log(el);
+// // const els = document.querySelectorAll(".intro");
+// const elsm = document.querySelectorAll("#chavan" , "#mansi");
+// console.log(els);
+const elements = document.querySelectorAll(".intro");
+elements.style.color="red";
+console.log(elements);
