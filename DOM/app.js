d73cc885c515425176c715b100e55840b0fa9e61
Mansi1Chavan/assignment <mansichavan5050@gmail.com>
2022-11-12 12:14:10 +0530

    Hello

--- DOM/app.js
+++ DOM/app.js
@@ -1,23 +1,36 @@
-// const elements = document.getElementsByClassName("some-para");
-// // const element = document.getElementsByClassName("some-para");
-// // console.log(elements)
-// // console.log(element)
 // console.log(document);
-// const ele  = document.getElementById("some-para")
-// console.log(ele.style);
+// const elements = document.getElementsByClassName("intro");
+// console.log(elements[0]);
+
+
+// const ele  = document.getElementById("deg")
 // ele.style.backgroundColor="Yellow";
-
-
-// console.log(document);
-// const ele = document.getElementById("deg1");
 // console.log(ele);
+
+
+// const elem = document.getElementById("deg1");
+// console.log(elem);
+
+
 // const element = document.getElementsByClassName("intro");
 // console.log(element[1]);
+
+
 // const el = document.querySelector("#chavan" , "#mansi");
 // console.log(el);
+
+
 // // const els = document.querySelectorAll(".intro");
 // const elsm = document.querySelectorAll("#chavan" , "#mansi");
 // console.log(els);
-const elements = document.querySelectorAll(".intro");
-elements.style.color="red";
-console.log(elements);
+
+
+// const elements = document.querySelectorAll("#deg");
+// elements.style.color="red";
+// console.log(elements);
+
+
+// const app = document.querySelector("#chavan");
+// document.body.style.color ="red";
+// app.style.color ="yellow";
+// console.log(app);
